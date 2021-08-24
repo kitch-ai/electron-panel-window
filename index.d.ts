14268baaa123abd73d08d34b0239e61a91472e4d
Ricardo Clerigo <rcclerigo@gmail.com>
2021-08-24 10:10:46 +0100

    typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare module 'kitch-ai/electron-panel-window' {
+    export function makeKeyWindow(window: any): boolean
+    export function makePanel(window: any): boolean
+    export function makeWindow(window: any): boolean
+}
