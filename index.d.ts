25e572379a2698033c0b4e66e840b9ad34ffdddf
Ricardo Clerigo <rcclerigo@gmail.com>
2021-08-24 10:12:04 +0100

    fix typo on ts definitions

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module 'kitch-ai/electron-panel-window' {
+declare module '@kitch-ai/electron-panel-window' {
     export function makeKeyWindow(window: any): boolean
     export function makePanel(window: any): boolean
     export function makeWindow(window: any): boolean
